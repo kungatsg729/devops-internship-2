fc342d3ad86ec8826c750486a0ab2535b896e318
kungatsg <kungatsg@gmail.com>
2026-06-11 07:27:41 +0000

    databases in a container

---
+++ linux/dbrun.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker compose -f ../postgresandmongo/docker-compose.yml up -d 
+echo "You are currently entering into mongodb container and may run your command queries accordingly. 
+If you wish to use postgresql instead, please exit and run the following command:
+docker exec -it postgres psql -U admin -d mydb"
+docker exec -it mongo mongosh -u admin -p password --authenticationDatabase admin --eval
