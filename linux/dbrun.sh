8a237e26749dfddcf698ee316a8eb8ab5a3272ff
kungatsg <kungatsg@gmail.com>
2026-06-12 08:17:24 +0000

    terraform basic

--- linux/dbrun.sh
+++ linux/dbrun.sh
@@ -3,4 +3,5 @@ docker compose -f ../postgresandmongo/docker-compose.yml up -d
 echo "You are currently entering into mongodb container and may run your command queries accordingly. 
 If you wish to use postgresql instead, please exit and run the following command:
 docker exec -it postgres psql -U admin -d mydb"
+sl
 docker exec -it mongo mongosh -u admin -p password --authenticationDatabase admin --eval
